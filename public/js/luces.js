3adae1aef602b930b9aefe0cbec6e1503e8b751f
Franco <francoherrera_8@hotmail.com.ar>
2022-03-30 17:43:10 -0300

    retoques

--- public/js/luces.js
+++ public/js/luces.js
@@ -1,16 +1,7 @@
 // ------------------------------------------------------
  
-  setInterval(flash, 1500);
+   setInterval(flash, 1500);
 function flash() {  
     document.querySelector("h1").classList.toggle("luces");
 };
-    
-
-
-/* 
-let nombre = document.getElementById ("nombre");
-let apellido = document.getElementsById("apellido");
-let edad = document.getElementsById("edad");
-let email = document.getElementsById("email");
-let telefono =document.getElementsById("telefono");
-let mensaje = document.getElementsById("mensaje");  */
+     
