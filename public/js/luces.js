610be08c6f760cbda7eb4f08bd10b544a86d4258
Franco <francoherrera_8@hotmail.com.ar>
2022-08-31 23:05:28 -0300

    app 2.1

--- public/js/luces.js
+++
@@ -1,7 +0,0 @@
-// ------------------------------------------------------
- 
-   setInterval(flash, 1500);
-function flash() {  
-    document.querySelector("h1").classList.toggle("luces");
-};
-     
